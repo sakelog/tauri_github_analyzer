fe8bff2c3a27f4c68830b00909eedf0a819961fd
sake <sakelog.website@gmail.com>
2022-04-25 20:52:04 +0900

    d.ts整理

---
+++ src/@types/github.d.ts
@@ -0,0 +1,19 @@
+declare namespace Github {
+  export type RepoInfo = {
+    name: string;
+    url: string;
+    views_info: TrafficInfo;
+    clones_info: TrafficInfo;
+  };
+
+  export type Traffic = {
+    timestamp: string;
+    uniques: number;
+    count: number;
+  };
+  export interface TrafficInfo {
+    count: number;
+    uniques: number;
+    items: Array<Traffic>;
+  }
+}
